238833d896c56d1c00493465a51a87ca71ec2ad0
Hanek Mukola <63111702+HanekM@users.noreply.github.com>
2020-04-06 11:26:38 +0300

    Create lesson03task01.py

---
+++ lesson03task01.py
@@ -0,0 +1,18 @@
+#  task 1
+msg = "Hanek M"
+print(msg)
+msg += "ukola"
+print(msg)
+hello = "Hello"
+print(hello, msg)
+hello += " "
+f_name = msg[:5]
+l_name = msg[6:]
+print(f_name, l_name)
+string = 'colorless'
+string = string[:4] + 'u' + string[4:]
+print(string)
+str1 = "dish-es"
+index = str1.find('-es')
+str1 = str1[:index]
+print(str1)
