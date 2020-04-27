b98f1e7df91b010f7c3bb27a3101795488e518b7
Hanek Mukola <63111702+HanekM@users.noreply.github.com>
2020-04-27 14:08:33 +0300

    Update lesson10task01-xx.py

--- lesson10task01-xx.py
+++ lesson10task01-xx.py
@@ -27,4 +27,44 @@ print(door1.status)
 
 
 # 2
+class Door():
+    """
+    Example for classmethod and how it works
+    """
+    colour = 'blue'
+    
+    def __init__(self, number, status):
+        self.number = number
+        self.status = status
+    
+    @classmethod
+    def paint(cls, colour):
+        cls.colour = colour
+    
+    def statusOpen(self):
+        self.status = 'open'
+        
+    def statusClose(self):
+        self.status = 'closed'
+    
+    def toggle(self):
+        if self.status == 'open':
+            self.status = 'closed'
+        else:
+            self.status = 'open'
+            
+door1 = Door(1,'closed')
+print(door1.status)
+door1.toggle()
+print(door1.status)
+
+print(door1.colour)
+door1.colour = 'red'
+print(door1.colour)
+
+print(Door.colour)
+Door.colour = 'red'
+print(Door.colour)
+Door.paint('white')
+print(Door.colour)
 
